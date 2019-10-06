a0a3a954de06712d941cd1c9638664218fbd7a56
PatrykMa <wiatraczekk@gmail.com>
2019-10-06 20:17:14 +0200

    reservation layout improve

--- app/src/main/java/pl/com/patryk/parcar/views/AddReservationFormDialogFragment.kt
+++ app/src/main/java/pl/com/patryk/parcar/views/AddReservationFormDialogFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
@@ -31,6 +32,15 @@ class AddReservationFormDialogFragment : DialogFragment() {
         binding.buttonCancel.setOnClickListener {
             dismiss()
         }
+        getDialog()!!.getWindow()!!.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE)
         return binding.root//inflater.inflate(R.layout.departure_dialog_fragment, container, false)
+    }
+
+    override fun onResume() {
+        val params = getDialog()!!.getWindow().getAttributes()
+        params.width = WindowManager.LayoutParams.MATCH_PARENT;
+        params.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        dialog!!.window!!.attributes = params as android.view.WindowManager.LayoutParams
+        super.onResume()
     }
 }
